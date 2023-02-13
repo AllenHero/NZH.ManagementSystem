06a07db59d119a98ce0b410e27aa7bfa6ee7e3e2
nizeheng <nizh@m3lean.com>
2023-02-13 15:38:19 +0800

    修改

---
+++ NZH.ManagementSystem/Control/TextBoxQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NZH.ManagementSystem.Control
+{
+    public class TextBoxQuery : TextBox
+    {
+        public new string Text
+        {
+            get
+            { return base.Text.Replace("'", "''"); }
+            set
+            { base.Text = value.Replace("''", "'"); }
+        }
+    }
+}

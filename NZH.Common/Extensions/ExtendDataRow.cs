80812c9e255447f3868bde25614c07e1d92e5161
nizeheng <nizh@m3lean.com>
2023-02-13 14:54:32 +0800

    修改

---
+++ NZH.Common/Extensions/ExtendDataRow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZH.Common.Extensions
+{
+    /// <summary>
+    /// 提供 DataRow 类的扩展方法。
+    /// </summary>
+    public static class ExtendDataRow
+    {
+        public static string ToString(this DataRow row, string columnName)
+        {
+            return row.Field<string>(columnName);
+        }
+
+        public static decimal? ToDecimal(this DataRow row, string columnName)
+        {
+            return row.Field<decimal?>(columnName);
+        }
+
+        public static DateTime? ToDateTime(this DataRow row, string columnName)
+        {
+            return row.Field<DateTime?>(columnName);
+        }
+
+        public static int? ToInt32(this DataRow row, string columnName)
+        {
+            return row.Field<int?>(columnName);
+        }
+    }
+}
